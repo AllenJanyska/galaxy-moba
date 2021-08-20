415ee87937045bd17caedc8dafc7857a230573b1
Allen <allenjany67@gmail.com>
2021-08-20 19:23:27 -0400

    base logic of turret ai

--- Assets/Scripts/TurretController/TurretController.cs
+++ Assets/Scripts/TurretController/TurretController.cs
@@ -9,16 +9,25 @@ public class TurretController : MonoBehaviour
     [SerializeField] private GameObject turretObject;
     private GameObject Target;
     [SerializeField] private GameObject ammo;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
+        //check target
+        //is target in range?
+        //fire
+        //otherwise
+        //is there another target?
+        //change target
+        //be done
+
+
+
     }
-}
